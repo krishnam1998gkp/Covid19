f7cbdfe0ea9c048af5eb1c8fa1a8bd6ee362f7ef
Krishnam <krishnam1998gkp@gmail.com>
2020-05-15 17:12:22 +0530

    Created static page homepage and info cards

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import HomePage from './Pages/homepage';
 
 class App extends Component{
   constructor(){
@@ -7,7 +8,7 @@ class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1>Krishnam</h1>
+        <HomePage />
       </div>
     )
   }
