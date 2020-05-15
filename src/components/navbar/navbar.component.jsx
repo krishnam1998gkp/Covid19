ccb21e8ebbc8d47b014856da8368ad97c083d775
aman bharti <amanbharti0302@gmail.com>
2020-05-16 02:44:30 +0530

    navbar without completely responsive in mobile view

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,25 +1,35 @@
-import React from 'react';
+/* eslint-disable no-useless-constructor */
+import React,{Component} from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.styles.scss';
 
-import {ReactComponent as Logo} from '../../assets/logo-nav.svg';
+import Logo from '../../assets/logo-nav.svg';
 
-const Navbarheader = () =>(
+class Navbarheader extends Component{
+    constructor(){
+        super();
+      }
+
+    render(){
+    return(
     <div className="section-navbar">
         <div className="navbar">
             <div className="navbar-logo">
-                <Logo></Logo>
+                <Link to="/"><img src={Logo} alt="LOGO" className="navbar-logo-img"></img></Link>
             </div>
-
             <div className="navbar-links">
-                    <Link href="#" className="navbar-link navbar-link__1">Hospitals</Link>
-                    <Link href="#" className="navbar-link navbar-link__2">Buy/sell</Link>
-                    <Link href="#" className="navbar-link navbar-link__3">Covid Help</Link>
-                    <Link href="#" className="navbar-link navbar-link__4">Create Account for free</Link>
+                    <Link href="#" className="navbar-link navbar-link__1 ">Hospitals</Link>
+                    <Link href="#" className="navbar-link navbar-link__2 ">Buy/sell</Link>
+                    <Link href="#" className="navbar-link navbar-link__3 ">Covid Help</Link>
+                    <Link href="#" className="navbar-link navbar-link__4 btn btn-green">Create Account for free</Link>
             </div>
         </div>
+        <div className="nav-btn-div">
+              <button className="nav-btn"></button>
+        </div>
     </div>
-)
-
+    )
+  }
+}
 
 export default Navbarheader;
