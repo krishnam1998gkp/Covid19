93e11de54434099fad72da0c785ea70fd92f4a27
aman bharti <amanbharti0302@gmail.com>
2020-05-17 01:18:15 +0530

    footer started

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -5,8 +5,15 @@ import './footer.styles.scss';
 const Footer = ()=>(
     <div className="section-footer">
          <div className="jj">
-            hjhjhjg
+             Come and expolre us
             <Rectbtn></Rectbtn>
+            <div>
+                <p>visit our github given below for project directory but don't claim it as own!</p>
+            </div>
+            <div>
+                <p>this website is developed under a react project by aman and krishnam</p>
+                <h4>&copy; copyright 2020</h4>
+            </div>
          </div>
     </div>
 )
